2625a22a88f11d366af658e14e5abe01b8a9360f
João Serafim <89697694+Serafimklm@users.noreply.github.com>
2023-06-14 20:42:48 -0300

    Sobre atributos: Os atributos criados nesta aula são:                             private String firstName;                             private String lastName;                             private String desiredJob;                             private String phoneNumber;                            é possível visualizar todos na classe MainActivity

--- app/src/main/java/com/seraffim/myapplication2/model/Person.java
+++ app/src/main/java/com/seraffim/myapplication2/model/Person.java
@@ -1,5 +1,12 @@
 package com.seraffim.myapplication2.model;
 
 public class Person {
+    //construtor = pessoa
+    // atributos = objetos - model - template
+    private String firstName;
+    private String lastName;
+    private String desiredJob;
+    private String phoneNumber;
+
     public Person (){}
 }
