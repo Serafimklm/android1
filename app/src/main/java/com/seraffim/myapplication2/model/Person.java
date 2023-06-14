c9d11fee6827c6c8b639e861a28fec9350106a7f
João Serafim <89697694+Serafimklm@users.noreply.github.com>
2023-06-13 22:08:04 -0300

    criação da classe Person localizada em Person.java e Visualizada em MainActivity.java

---
+++ app/src/main/java/com/seraffim/myapplication2/model/Person.java
@@ -0,0 +1,5 @@
+package com.seraffim.myapplication2.model;
+
+public class Person {
+    public Person (){}
+}
--- app/src/main/java/com/seraffim/myapplication2/view/MainActivity.java
+++ app/src/main/java/com/seraffim/myapplication2/view/MainActivity.java
@@ -5,12 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.seraffim.myapplication2.R;
+import com.seraffim.myapplication2.model.Person;
 
 public class MainActivity extends AppCompatActivity {
+
+    Person person;
+    // acima esta usando person, que é a calsse pessoa e objeto pessoa.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+            // Person = New person, ou seja, um novo objeto foi criado aqui.
+        person = new Person();
+
     }
 }
